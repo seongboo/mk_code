fcd1a0d0a1b66cb503b4852a3aa6ab4e60341d53
Seong Boo's mac <sbpark@Seongui-MacBookPro.local>
2022-10-05 21:13:24 +0900

    coding start

---
+++ VASP/analysis.py
@@ -0,0 +1,12 @@
+# analysis of DOS, bandstructure, local electrostatic potential, etc..
+import numpy as np
+
+
+
+print("  DOS | Band | Local_electro_potential\n")
+
+keyword = input("choose")
+print(keyword)
+# not here, error~
+
+
---
+++ VASP/io.py
@@ -0,0 +1,16 @@
+import numpy 
+
+class ImportData(self):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def DOSCAR(self):
+
+    def PROCAR(self):
+
+    def OUTCAR(self):
+
+    def POSCAR(self):
+        
+
